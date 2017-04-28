e376aea7be9fdf9ce2fa89a1d095d6389304624b
Linda Xia <lindax@andrew.cmu.edu>
2017-04-27 20:37:42 -0400

    Added controller actions to OrdersController

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,33 @@
 class OrdersController < ApplicationController
+	include ChessStoreHelpers::Cart
+	
+	def index 
+		if current_user.role?(:customer)
+			@orders = current_user.orders.chronological.to_a
+		else 
+			@orders = Order.chronological.to_a
+		end
+	end
+
+	def new 
+		@order = Order.new
+	end
+
+	def edit
+	end
+
+	def create
+
+	end
+
+	def destroy 
+		@order.destroy
+
+	end
+
+	private 
+
+	def order_params
+		params.require(:order).permit(:date, :school_id, :user_id, :grand_total, :payment_receipt)
+	end
 end
