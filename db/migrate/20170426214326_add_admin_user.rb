af3a54976ad2a60668b026d1b2cf584996142110
Linda Xia <lindax@andrew.cmu.edu>
2017-04-29 00:52:37 -0400

    Made a new partial for user information

--- db/migrate/20170426214326_add_admin_user.rb
+++ db/migrate/20170426214326_add_admin_user.rb
@@ -1,8 +1,8 @@
 class AddAdminUser < ActiveRecord::Migration
 	def up
 	    admin = User.new
-	    admin.first_name = "Admin"
-	    admin.last_name = "Admin"
+	    admin.first_name = "Linda"
+	    admin.last_name = "Xia"
 	    admin.email = "admin@example.com"
 	    admin.username = "admin"
 	    admin.password = "secret"
