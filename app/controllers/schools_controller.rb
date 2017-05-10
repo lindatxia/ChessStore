30ffae31ab51a58b6382df953d430dab2997e435
Linda Xia <lindax@andrew.cmu.edu>
2017-05-09 22:33:00 -0400

    Added schools for admin functionality

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -8,7 +8,24 @@ class SchoolsController < ApplicationController
 	end
 
 	def new 
+		@school = School.new
+	end
 
+	def create 
+		@school = School.new(school_params)
+		if @school.save 
+			flash[:notice] = "Successfully saved #{@school.name}!"
+			redirect_to schools_path
+		else 
+			render action: 'new'
+		end
+	end
+
+
+	private 
+
+	def school_params 
+		params.require(:school).permit(:name, :street_1, :street_2, :city, :state, :zip, :min_grade, :max_grade, :active)
 	end
 
 end
